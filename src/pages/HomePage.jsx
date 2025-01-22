5c61f0e983a08cdd8d208ca509bd187a8c7757ad
JudiPARK0416 <p.eunjung.0425@gmail.com>
2025-01-22 23:28:29 +1100

    feat/homepage Hero Section with Searchbox UI implemented

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,33 @@
 import { useAuth } from "../contexts/AuthContext/useAuth"; // Import the useAuth hook
+import SearchInput from "../features/Events/components/SearchInput";
 
 const HomePage = () => {
   const { loading, isAuthenticated } = useAuth();
 
   console.log({ loading, isAuthenticated });
 
-  return <div>HomePage</div>;
+  return (
+    <>
+      <main>
+        {/* Search Box */}
+        <div className="relative w-full bg-[url('/images/Hero.jpeg')] bg-cover bg-center p-20 pt-30 flex flex-col justify-center items-center">
+          <div className="absolute inset-0 bg-[#353345] opacity-70"></div>
+
+          <div className="relative w-full md:w-1/2">
+            <p className="text-lg lg:text-3xl font-bold text-white text-left">
+              Don't miss out!
+              <br /> Explore the{" "}
+              <span className="text-[#FFE047]">vibrant events</span> happening
+              locally and globally.
+            </p>
+            <div className="mt-10">
+              <SearchInput />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
