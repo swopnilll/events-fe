11a09808eb0bba2f63dfaec0f42633af770d4c9e
JudiPARK0416 <p.eunjung.0425@gmail.com>
2025-01-23 00:13:04 +1100

    feat/homepage Banners Completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -25,6 +25,59 @@ const HomePage = () => {
             </div>
           </div>
         </div>
+
+        {/* <!-- Create Event Banner --> */}
+        <section
+          className="w-full bg-cover bg-no-repeat bg-right md:bg-left py-10 px-6 bg-[#2B293D] text-[#ffe047]"
+          style={{
+            backgroundImage: "url('/assets/images/create-event-banner.svg')",
+          }}
+        >
+          <div className="text-center">
+            <h4 className="text-lg font-bold">
+              Create an event with Eventify
+            </h4>
+            <p className="text-sm font-light">
+              Got a show, event, activity or a great experience? Partner with us
+              & get listed on Eventify
+            </p>
+          </div>
+
+          <div className="flex justify-center mt-6">
+            <button className="flex items-center bg-[#ffe047] text-black py-2 px-6 rounded-lg">
+              {/* <img
+                src="assets/images/create-event-button.svg"
+                alt=""
+                class="h-6 mr-2"
+              /> */}
+              <span className="font-bold">Create Event</span>
+            </button>
+          </div>
+        </section>
+
+        {/* <!-- Newsletter Section --> */}
+        <section className="w-full bg-[#ffe047] flex flex-col items-center py-9 px-6">
+          <div className="text-center mb-4">
+            <h3 className="text-lg text-[#2d2c3c] font-bold">
+              Subscribe to our Newsletter
+            </h3>
+            <p className="text-sm text-[#2d2c3ce7] font-light">
+              Receive our weekly newsletter & updates with new events from your
+              favourite organizers & venues.
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-3">
+            <input
+              type="email"
+              className="w-64 p-3 rounded-md outline-none border-gray-300"
+              placeholder="Enter your e-mail address"
+            />
+            <button className="bg-[#2b293d] text-[#ffe047] px-6 py-3 rounded-md">
+              Subscribe
+            </button>
+          </div>
+        </section>
       </main>
     </>
   );
