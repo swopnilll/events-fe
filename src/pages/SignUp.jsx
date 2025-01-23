1768c154fde98c5fc82f1eb7c81c342a65fbc5e9
suraj regmi <hyakoste@gmail.com>
2025-01-24 00:43:49 +1100

    fix logo component on signup page.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,157 +7,151 @@ import { XIcon } from "lucide-react";
 
 import InputField from "../components/InputField/InputField";
 
+import Logo from "../components/logo/Logo";
+
 const SignUp = ({}) => {
-	const [formData, setFormData] = useState({
-		fullName: "",
-		email: "",
-		password: "",
-	});
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-	const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleBack = () => {
-		navigate(-1); // Goes back to the previous page
-	};
+  const handleBack = () => {
+    navigate(-1); // Goes back to the previous page
+  };
 
-	// Handle input changes
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({ ...formData, [name]: value });
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
 
-		// Clear error for the field being modified
-		setErrors((prevErrors) => ({
-			...prevErrors,
-			[name]: "",
-		}));
-	};
+    // Clear error for the field being modified
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
 
-	// Validate inputs
-	const validate = () => {
-		const newErrors = {};
+  // Validate inputs
+  const validate = () => {
+    const newErrors = {};
 
-		if (!formData.email) {
-			newErrors.email = "Email is required.";
-		} else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-			newErrors.email = "Enter a valid email address.";
-		}
+    if (!formData.email) {
+      newErrors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Enter a valid email address.";
+    }
 
-		if (!formData.password) {
-			newErrors.password = "Password is required.";
-		} else if (formData.password.length < 6) {
-			newErrors.password = "Password must be at least 6 characters.";
-		}
+    if (!formData.password) {
+      newErrors.password = "Password is required.";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters.";
+    }
 
-		if (!formData.fullName) {
-			newErrors.fullName = "Full name is required.";
-		} else if (formData.fullName.trim().split(" ").length < 2) {
-			newErrors.fullName = "Please enter your full name (first and last name).";
-		}
+    if (!formData.fullName) {
+      newErrors.fullName = "Full name is required.";
+    } else if (formData.fullName.trim().split(" ").length < 2) {
+      newErrors.fullName = "Please enter your full name (first and last name).";
+    }
 
-		return newErrors;
-	};
+    return newErrors;
+  };
 
-	// Handle form submission
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		const validationErrors = validate();
+    const validationErrors = validate();
 
-		if (Object.keys(validationErrors).length > 0) {
-			setErrors(validationErrors);
-			return;
-		}
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
 
-		// Call an API or perform login action
-		console.log("Register successful", formData);
+    // Call an API or perform login action
+    console.log("Register successful", formData);
 
-		setErrors({});
-	};
+    setErrors({});
+  };
 
-	return (
-		<>
-			<div className="w-screen h-screen flex flex-col lg:flex-row">
-				{/* Graphics Section */}
-				<div className="hidden bg-[#2B293D] lg:block lg:w-2/4 p-6 text-white text-3xl">
-				<img
-					src="/images/Logo2.svg"
-					alt="Elegant Event Logo"
-					className="max-h-full w-auto"
-				/>
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col lg:flex-row">
+        {/* Graphics Section */}
+        <div className="hidden bg-[#2B293D] lg:block lg:w-2/4 p-6 text-white text-3xl">
+          <Logo className="h-20" />
 
+          <div className="text-white text-3xl font-bold flex pt-32">
+            <div>
+              <p>Discover tailored events.</p>
+              <p>Sign in for personalized recommendations today!</p>
+            </div>
+          </div>
+        </div>
 
-					<div className="text-white text-3xl font-bold flex pt-32">
-						<div>
-							<p>Discover tailored events.</p>
-							<p>Sign in for personalized recommendations today!</p>
-						</div>
-					</div>
-				</div>
+        {/* Login Form */}
+        <div className="p-6 bg-white h-full shadow-lg lg:w-3/4 flex flex-col items-center justify-around">
+          <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-left w-3/4">
+            Create Account
+          </h1>
 
-				{/* Login Form */}
-				<div className="p-6 bg-white h-full shadow-lg lg:w-3/4 flex flex-col items-center justify-around">
-					<h1 className="text-2xl font-semibold text-gray-800 mb-6 text-left w-3/4">
-						Create Account
-					</h1>
+          <form onSubmit={handleSubmit} className="h-3/4 w-3/4 flex flex-col">
+            <InputField
+              label="Full Name"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              placeholder="Enter your full name"
+              error={errors.fullName}
+            />
 
-					<form
-						onSubmit={handleSubmit}
-						className="h-3/4 w-3/4 flex flex-col"
-					>
-						<InputField
-							label="Full Name"
-							name="fullName"
-							value={formData.fullName}
-							onChange={handleChange}
-							placeholder="Enter your full name"
-							error={errors.fullName}
-						/>
+            <InputField
+              label="Email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+              error={errors.email}
+            />
 
-						<InputField
-							label="Email"
-							type="email"
-							name="email"
-							value={formData.email}
-							onChange={handleChange}
-							placeholder="Enter your email"
-							error={errors.email}
-						/>
+            <InputField
+              label="Password"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+              error={errors.password}
+            />
 
-						<InputField
-							label="Password"
-							type="password"
-							name="password"
-							value={formData.password}
-							onChange={handleChange}
-							placeholder="Enter your password"
-							error={errors.password}
-						/>
+            <button
+              type="submit"
+              className="mt-4 w-full px-4 py-2 bg-[#2b293dd3] text-white font-semibold rounded-lg shadow hover:bg-[#2b293d] transition duration-300"
+            >
+              Create Account
+            </button>
 
-						<button
-							type="submit"
-							className="mt-4 w-full px-4 py-2 bg-[#2b293dd3] text-white font-semibold rounded-lg shadow hover:bg-[#2b293d] transition duration-300"
-						>
-							Create Account
-						</button>
-
-						<div className="text-[#636363ce] text-sm mt-6">
-							<span>Already have an account? </span>
-							<span className="cursor-pointer text-[#636363] font-bold">
-								{" "}
-								Sign In
-							</span>
-						</div>
-					</form>
-				</div>
-			</div>
-			<XIcon
-				className="size-6 text-black absolute right-6 top-4 cursor-pointer"
-				onClick={handleBack}
-			/>
-		</>
-	);
+            <div className="text-[#636363ce] text-sm mt-6">
+              <span>Already have an account? </span>
+              <span className="cursor-pointer text-[#636363] font-bold">
+                {" "}
+                Sign In
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+      <XIcon
+        className="size-6 text-black absolute right-6 top-4 cursor-pointer"
+        onClick={handleBack}
+      />
+    </>
+  );
 };
 
 SignUp.propTypes = {};
